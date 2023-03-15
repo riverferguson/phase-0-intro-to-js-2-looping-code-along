fc125db46e9b106399ebeb44b547037c6cecfc16
River Ferguson <riverferguson@gmail.com>
2023-03-15 10:37:32 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(array, event) {
+    let output = []
+    for (let i = 0; i < array.length; i++) {
+        output.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+
+    }
+    return output 
+}
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+function countDown() {
+    let count = 10
+    while (count >= 0) {
+        console.log(count--)
+    }
+}
+
